23d4262242f95332473517ca7365e02600271606
leonelVc19 <vazquez.juan.1fm@gmail.com>
2024-02-23 18:49:15 -0600

    Proyecto de Mapas

---
+++ src/app/maps/components/mini-maps/mini-maps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-maps',
+  templateUrl: './mini-maps.component.html',
+  styleUrls: ['./mini-maps.component.css']
+})
+export class MiniMapsComponent {
+
+}
